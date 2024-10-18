87f7d2ab1168301a5c8611cabd8f70ae1a5335af
loganrains <logankrains@gmail.com>
2024-10-18 17:15:20 -0500

    added new logo to the top of the hamburger menu, currently displaying alt text, looking for fix

--- project/src/components/Sidebar.jsx
+++ project/src/components/Sidebar.jsx
@@ -4,7 +4,10 @@ import '../Sidebar.css';
 export default function Sidebar() {
     return(
         <Menu>
-            
+            <a className="menu-item" href="/">
+                <img src="assets/Personal_Logo_2.png" alt="Orange Hexagon with black border containing a white L and a black R pushed together where the first leg of the R is the long side of the L"/>
+            </a>
+
             <a className="menu-item" href="/">
                 Home
             </a>
